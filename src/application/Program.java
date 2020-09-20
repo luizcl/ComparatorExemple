d2c5e05302b4ca81e729665c53f207c1e0b3ed43
Luiz <luiz.cl@live.com>
2020-09-20 09:15:55 -0300

    Lambda 3 - With less code and direct anonymous function

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -16,9 +15,7 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("TV", 900.0));
 		
-		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		
-		list.sort(comp);
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for(Product p : list) {
 			System.out.println(p);
