b3dbc3501b54287c6a374d637f8ec30379917672
Luiz <luiz.cl@live.com>
2020-09-20 09:07:16 -0300

    Lambda 1 - With anonymous function

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,12 +16,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("TV", 900.0));
 		
-		Comparator<Product> comp = new Comparator<Product>() {
-
-			@Override
-			public int compare(Product o1, Product o2) {
-				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
-			}
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
 		
 		list.sort(comp);
